019977a6310a8467122cf0aec3a88eaccc570400
panikedpineapple <74149183+panikedpineapple@users.noreply.github.com>
2021-03-10 00:10:02 +0530

    fixe old data and removed keys

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ import os
 COMMAND_PREFIX = "!"
 
 # The bot token. Keep this secret!
-BOT_TOKEN = "ODE4NDcwNjkzNDI2OTU0Mjcx.YEYiOw.RGKdynuguh4gAkWWpMJ2YPtcKhY"
+BOT_TOKEN = ""
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
 NOW_PLAYING = COMMAND_PREFIX + "commands"
@@ -13,6 +13,6 @@ NOW_PLAYING = COMMAND_PREFIX + "commands"
 # Base directory. Feel free to use it if you want.
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-ALLOWED_CHANNELS = [818470383513763853]
+ALLOWED_CHANNELS = []
 
-USER_WATCHLIST = [225911806034575361]
+USER_WATCHLIST = []
